27d634d2be5159b7c0f9a3b03706bc32ec15b6c1
RafaelJohn9 <rafaeljohb@gmail.com>
2023-08-29 15:24:20 +0300

    task 26: completed

---
+++ 112-array_to_bst.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst- builds a binary search tree from an array
+ * @array: array to be searched
+ * @size: size of the array
+ * Return: a ptr to the array head
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i = 0;
+	bst_t *root;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		bst_insert(&root, array[i]);
+		i++;
+	}
+	return (root);
+}
