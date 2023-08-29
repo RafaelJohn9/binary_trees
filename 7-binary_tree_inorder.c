b53725a0052ab2d046b166aa9d181d2356490243
RafaelJohn9 <rafaeljohb@gmail.com>
2023-08-29 12:47:03 +0300

    task 7,task 7,8 debugged

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -20,12 +20,12 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (tree->left)
 	{
-		binary_tree_inorder(tree->left, &print_num);
+		binary_tree_inorder(tree->left, func);
 		func(tree->n);
 	}
 	if (tree->right)
 	{
-		binary_tree_inorder(tree->right, &print_num);
+		binary_tree_inorder(tree->right, func);
 		if (!tree->left)
 		{
 			func(tree->n);
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -20,11 +20,11 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (tree->left)
 	{
-		binary_tree_postorder(tree->left, &print_num);
+		binary_tree_postorder(tree->left, func);
 	}
 	if (tree->right)
 	{
-		binary_tree_postorder(tree->right, &print_num);
+		binary_tree_postorder(tree->right, func);
 	}
 	func(tree->n);
 }
