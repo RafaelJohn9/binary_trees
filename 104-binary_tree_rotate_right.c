bdccfffe5cb6cbb04a009972dc96b4f0be4df03d
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-29 13:21:31 +0300

    finished

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	binary_tree_t *new_root;
+
 	if (!tree || !tree->left)
 		return (tree);
-
-	binary_tree_t *new_root;
 
 	new_root = tree->left;
 	tree->left = new_root->right;
