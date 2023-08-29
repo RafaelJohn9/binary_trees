bf17d158877461bb1d135d5fa967354587b5e87b
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-29 18:55:29 +0300

    added task 0 and 1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,23 +1,25 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_node- a function that creates a binary tree node
- * @parent: the parent node
- * @value: value of the node
- * Return: a binary_tree_t
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to put in the new node
+ * Return: Pointer to the new node, or NULL on failure
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *child;
+	binary_tree_t *new_node;
 
-	child = malloc(sizeof(binary_tree_t));
-	if (!child)
-	{
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
-	child->n = value;
-	child->parent = parent;
-	child->left = NULL;
-	child->right = NULL;
-	return (child);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,36 +1,29 @@
 #include "binary_trees.h"
 
+
 /**
- * binary_tree_insert_left- inserts a node on the left branch
- * @parent: this is the parent node
- * @value: the value of the node
- * Return: a binary_tree_t
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * @parent: Pointer to the node to insert the left-child in
+ * value: Value to store in the new node
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *child;
+	binary_tree_t *new_node;
 
-	if (!parent)
-	{
+	if (parent == NULL)
 		return (NULL);
-	}
-
-	child = malloc(sizeof(binary_tree_t));
-	if (!child)
-	{
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 		return (NULL);
-	}
-	child->n = value;
-	child->parent = parent;
-	child->left = NULL;
-	child->right = NULL;
 
 	if (parent->left != NULL)
 	{
-		parent->left->parent = child;
-		child->left = parent->left;
-
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
-	parent->left = child;
-	return (child);
+
+	parent->left = new_node;
+	return (new_node);
 }
