febf14093a7feed8304f1728abda6fb3f161ebb7
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-29 11:31:34 +0300

    finished

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,12 +12,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *grandparent;
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-		return NULL;
+		return (NULL);
 	grandparent = node->parent->parent;
 	if (grandparent->left == node->parent)
-		return grandparent->right;
+		return (grandparent->right);
 	else if (grandparent->right == node->parent)
-		return grandparent->left;
+		return (grandparent->left);
 	else
 		return (NULL);
 }
