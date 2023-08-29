0acc86afe82f9fc78f37cd342f65bce1bfd3063a
kaleabendrias <kaleabendrias1212@gmail.com>
2023-08-29 20:43:36 +0300

    finished

--- binary_trees.h
+++ binary_trees.h
@@ -72,5 +72,11 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
+size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 #endif
